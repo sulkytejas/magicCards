1da26e4cb1784f911f5ae0713f0e724996a3bbb8
tejas Pashte <sulkytejas@yahoo.co.in>
2017-12-12 08:29:33 -0800

    single deck

---
+++ components/singleDeck.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default class SingleDeck extends React.Component{
+  state={ }
+
+  render(){
+
+    return(
+      <View>
+        <Text>Single Deck</Text>
+      </View>
+    )
+  }
+}
