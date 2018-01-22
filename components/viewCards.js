0e1b8a7e20bc85a17d9d74f5142c82cc7c6be1d9
Tejas Pashte <tejas@MCHQ-99-MBP.local>
2018-01-22 13:00:29 -0800

    New Card

--- components/viewCards.js
+++ components/viewCards.js
@@ -14,7 +14,6 @@ export default class ViewCards extends React.Component{
   componentDidMount(){
       this.setState({refreshing: true});
       let key = this.props.navigation.state.params.entryTitle
-      // getCards({key}).then(data=> console.log(data))
       getCards({key}).then(data=> this.setState({cards:data}))
 }
 
@@ -33,71 +32,82 @@ export default class ViewCards extends React.Component{
 
   render(){
     let {cards} = this.state
-    console.log(cards)
-    return(
-      // <View>
-      //
-      //   {cards.map((result,index)=>(
-      //     <View key={index}>
-      //       <Text>
-      //          QUESTION: {result.question}
-      //       </Text>
-      //       <Text>
-      //          Answer: {result.answer}
-      //       </Text>
-      //       <TouchableOpacity >
-      //           <Text>Correct</Text>
-      //       </TouchableOpacity>
-      //       <TouchableOpacity >
-      //           <Text>Wrong</Text>
-      //       </TouchableOpacity>
-      //     </View>
-      //
-      //   ))}
-      //
-      // </View>
+    console.log(cards.length)
+    if (cards.length > 0) {
+      return(
+        // <View>
+        //
+        //   {cards.map((result,index)=>(
+        //     <View key={index}>
+        //       <Text>
+        //          QUESTION: {result.question}
+        //       </Text>
+        //       <Text>
+        //          Answer: {result.answer}
+        //       </Text>
+        //       <TouchableOpacity >
+        //           <Text>Correct</Text>
+        //       </TouchableOpacity>
+        //       <TouchableOpacity >
+        //           <Text>Wrong</Text>
+        //       </TouchableOpacity>
+        //     </View>
+        //
+        //   ))}
+        //
+        // </View>
 
-      <Container>
+
+
+        <Container>
+          <View>
+            <DeckSwiper
+              ref={(c) => this._deckSwiper = c}
+              dataSource={this.state.cards}
+              renderEmpty={() =>
+                <View style={{ alignSelf: "center" }}>
+                  <Text>{this.state.totalCount}</Text>
+                </View>
+              }
+              renderItem={ item =>
+                <Card style={{ elevation: 3 }}>
+                  <CardItem>
+                    <Left>
+                      <Body>
+                        <Text>{item.question}</Text>
+                        <Text note>NativeBase</Text>
+                        <Button success full onPress={this.Increment} style={{margin:20}}><Text>Correct</Text></Button>
+                        <Button danger full onPress={this.Decrement} style={{margin:20}}><Text>Incorrect</Text></Button>
+                      </Body>
+                    </Left>
+                  </CardItem>
+                  <CardItem>
+                    <Icon name="heart" style={{ color: '#ED4A6A' }} />
+                    <Text>{item.answer}</Text>
+                  </CardItem>
+                </Card>
+              }
+            />
+          </View>
+          <View style={{ flexDirection: "row", flex: 1, position: "absolute", bottom: 50, left: 0, right: 0, justifyContent: 'space-between', padding: 15 }}>
+            <Button iconLeft onPress={() => this._deckSwiper._root.swipeLeft()}>
+              <Icon name="arrow-back" />
+              <Text>Swipe Left</Text>
+            </Button>
+            <Button iconRight onPress={() => this._deckSwiper._root.swipeRight()}>
+              <Icon name="arrow-forward" />
+              <Text>Swipe Right</Text>
+            </Button>
+          </View>
+        </Container>
+      )
+    } else {
+      return (
         <View>
-          <DeckSwiper
-            ref={(c) => this._deckSwiper = c}
-            dataSource={this.state.cards}
-            renderEmpty={() =>
-              <View style={{ alignSelf: "center" }}>
-                <Text>{this.state.totalCount}</Text>
-              </View>
-            }
-            renderItem={ item =>
-              <Card style={{ elevation: 3 }}>
-                <CardItem>
-                  <Left>
-                    <Body>
-                      <Text>{item.question}</Text>
-                      <Text note>NativeBase</Text>
-                      <Button success full onPress={this.Increment} style={{margin:20}}><Text>Correct</Text></Button>
-                      <Button danger full onPress={this.Decrement} style={{margin:20}}><Text>Incorrect</Text></Button>
-                    </Body>
-                  </Left>
-                </CardItem>
-                <CardItem>
-                  <Icon name="heart" style={{ color: '#ED4A6A' }} />
-                  <Text>{item.answer}</Text>
-                </CardItem>
-              </Card>
-            }
-          />
+          <Text> Cards are loading </Text>
         </View>
-        <View style={{ flexDirection: "row", flex: 1, position: "absolute", bottom: 50, left: 0, right: 0, justifyContent: 'space-between', padding: 15 }}>
-          <Button iconLeft onPress={() => this._deckSwiper._root.swipeLeft()}>
-            <Icon name="arrow-back" />
-            <Text>Swipe Left</Text>
-          </Button>
-          <Button iconRight onPress={() => this._deckSwiper._root.swipeRight()}>
-            <Icon name="arrow-forward" />
-            <Text>Swipe Right</Text>
-          </Button>
-        </View>
-      </Container>
-    )
+      )
+    }
+
   }
 }
